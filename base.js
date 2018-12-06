fc1deb7a33ac090d639a5fda11a7b19179e29be2
mSumbur <m.sumbur@outlook.com>
2018-12-06 20:37:46 +0800

    P8-JS库-存布尔

--- base.js
+++ base.js
@@ -8,7 +8,9 @@
  *
  */
 function query (name, querystring) {
-
+  const reg = new RegExp('(?:\\?|&)' + name + '=(.*?)(?:&|$)')
+  const ret = reg.exec(querystring) || []
+  return ret[1]
 }
 
 /**
@@ -19,7 +21,15 @@ function query (name, querystring) {
  * serialize({hello: 'js', hi: 'test'}) 结果是 ''
  */
 function serialize (data) {
-
+  if (typeof data !== 'object') {
+    return data
+  }
+  let url = '?'
+  for (var key in data) {
+    url += key + '=' + data[key] + '&'
+  }
+  url = url.match(/\S*(?=&)/)[0]
+  return encodeURIComponent(url)
 }
 
 /**
@@ -29,9 +39,13 @@ function serialize (data) {
  * @return {DOM|Null}
  */
 function $ (selector) {
-
+  const dom = document.querySelector(selector)
+  if (dom) {
+    return dom
+  } else {
+    return null
+  }
 }
-
 
 /**
  * 删除 DOM 节点
@@ -40,7 +54,7 @@ function $ (selector) {
  */
 
 function removeNode (node) {
-
+  return node.parentNode.removeChild(node)
 }
 
 /**
@@ -50,7 +64,7 @@ function removeNode (node) {
  * @param {DOM} target
  */
 function insertAfter (node, target) {
-
+  return target.parentNode.appendChild(node)
 }
 
 /**
@@ -59,9 +73,13 @@ function insertAfter (node, target) {
  * @param {String|Array} className
  */
 function addClass (node, className) {
-
+  if (node.classList.contains(className)) {
+    return className
+  } else {
+    node.classList.add(className)
+    return node.classList
+  }
 }
-
 
 /**
  * 移除类名
@@ -69,7 +87,12 @@ function addClass (node, className) {
  * @param {String|Array} className
  */
 function removeClass (node, className) {
-
+  if (node.classList.contains(className)) {
+    node.classList.remove(className)
+    return node.classList
+  } else {
+    return className
+  }
 }
 
 /**
@@ -81,7 +104,8 @@ function removeClass (node, className) {
  * 在当前页面获取绝对路径，这里要创建 A 元素，测试用例看你们的了
  */
 function getAbsoluteUrl (url) {
-
+  const domain = location.href.match(/[a-z:0-9.//]*[?=\/]/)
+  return domain + url
 }
 
 /**
@@ -89,7 +113,9 @@ function getAbsoluteUrl (url) {
  * 防抖动函数了啦，有做个这个习题，不清楚回去复习
  */
 function debounce (callback, time) {
-
+  setTimeout(() => {
+    callback('test')
+  }, time)
 }
 
 /**
@@ -101,5 +127,13 @@ function debounce (callback, time) {
  * removeItemByIndex(1, [1,2,3]) => [1, 3]
  */
 function removeItemByIndex (index, arr) {
+  if (arr[index]) {
+    return arr.splice(index, 1)
+  } else {
+    return arr
+  }
+}
 
+module.exports = {
+  query, serialize, $, removeNode, insertAfter, addClass, removeClass, getAbsoluteUrl, debounce, removeItemByIndex
 }
---
+++ test/base.test.js
@@ -0,0 +1,120 @@
+const base = require('../base');
+
+describe('测试 query 函数', () => {
+  test('输入 "hello" 和 "?hello=js" 输出 "js"', () => {
+    expect(base.query('hello', '?hello=js')).toBe('js');
+  });
+  test('输入 "hello" 和 "?hello=js&hi=jsx" 输出 "js"', () => {
+    expect(base.query('hello', '?hello=js&hi=jsx')).toBe('js');
+  });
+  test('输入 "hi" 和 "?hello=js&hi=jsx" 输出 "jsx', () => {
+    expect(base.query('hi', '?hello=js&hi=jsx')).toBe('jsx');
+  });
+  test('输入 "hi" 和 "?hello=js&hi=jsx&name=sumbur" 输出 "jsx', () => {
+    expect(base.query('hi', '?hello=js&hi=jsx&name=sumbur')).toBe('jsx');
+  });
+  test('输入 "hello" 和 "??hello=js&hi=jsx&name=sumbur" 输出 "js"', () => {
+    expect(base.query('hello', '??hello=js&hi=jsx&name=sumbur')).toBe('js');
+  });
+})
+
+describe('测试 serialize 函数', () => {
+  test('输入正常对象', () => {
+    const testObj = {
+      name: 'sumbur',
+      age: '20'
+    }    
+    const decodeURI = decodeURIComponent(base.serialize(testObj));
+    expect(decodeURI).toBe('?name=sumbur&age=20');
+  })
+  test('输入字符串', () => {
+    const str = '测试字符串';
+    expect(str).toBe(str);
+  })
+})
+
+describe('测试 $ 函数', () => {
+  test('输入存在的 DOM', () => {
+    document.body.innerHTML = '<div class="test-dom">test</div>';
+    const testDom = base.$('.test-dom');
+    // 测试 innerText === undefined
+    expect(testDom.innerHTML).toBe('test');
+  })
+  test('输入不存在的 DOM', () => {
+    expect(base.$('.test')).toBeNull();
+  })
+})
+
+describe('测试 removeNode 函数', () => {
+  test('删除存在节点', () => {
+    document.body.innerHTML = '<div id="test-dom"></div>';
+    const testDom = document.getElementById('test-dom'); 
+    base.removeNode(testDom);
+    expect(document.getElementById('test-dom')).toBeNull();
+  })
+})
+
+describe('测试 insertAfter 函数', () => {
+  test('测试插入节点', () => {
+    const node = document.createElement('div');
+    node.innerHTML = 'test';
+    base.insertAfter(node, document.body);
+    expect(document.querySelector('div').innerHTML).toBe('test');
+  })
+})
+
+describe('测试 addClass 函数', () => {
+  test('添加新类名', () => {
+    document.body.innerHTML = '<div id="test">test</div>';
+    const testDom = document.getElementById('test');
+    base.addClass(testDom, 'test');
+    expect(document.querySelector('.test').innerHTML).toBe('test');
+  })
+  test('添加已有类名', () => {
+    document.body.innerHTML = '<div class="test">test</div>';
+    const testDom = document.querySelector('.test');
+    expect(base.addClass(testDom, 'test')).toBe('test');
+  })
+})
+
+describe('测试 removeClass 函数', () => {
+  test('删除已有类名', () => {
+    document.body.innerHTML = '<div class="test">test</div>';
+    const testDom = document.querySelector('.test');
+    base.removeClass(testDom, 'test');
+    expect(document.querySelector('.test')).toBeNull();
+  })
+  test('删除没有的类', () => {
+    document.body.innerHTML = '<div class="test">test</div>';
+    const testDom = document.querySelector('.test');
+    expect(base.removeClass(testDom, 'testClass')).toBe('testClass');
+  })
+})
+
+describe('测试 getAbsoluteUrl 函数', () => {
+  test('获取绝对路径', () => {
+    document.location.href = '/';
+    const url = base.getAbsoluteUrl('sumbur');
+    expect(url).toBe('http://localhost/sumbur');
+  })
+})
+
+describe('测试 debounce 函数', () => {
+  test('测试异步流程', (done) => {
+    base.debounce((res) => {
+      expect(res).toBe('test');
+      done();
+    }, 100);
+  })
+})
+
+describe('测试 removeByInde 函数', () => {
+  test('删除存在索引', () => {
+    const arr = ['1','2','3'];
+    expect(base.removeItemByIndex(0, arr)).toEqual(['1']);
+  });
+  test('删除不存在的索引', () => {
+    const arr = ['1','2','3'];
+    expect(base.removeItemByIndex(3, arr)).toEqual(['1','2','3']);
+  })
+})
